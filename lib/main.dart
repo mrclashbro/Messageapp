dc5db349ca4dd10a2f9c5c9e9992f7805128ae00
nav1996 <2017cs122@stu.ucsc.cmb.ac.lk>
2020-10-21 19:32:13 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:message_app/providers/MessageProvider.dart';
+//import 'package:msgschedule_2/providers/SettingsProvider.dart';
+
+import 'pages/schedule/SchedulePage.dart';
+
+
+
+void main() => runApp(MyApp());
+
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
+  final String _appName = 'MsgSchedule';
+  Widget _home;
+
+  _MyAppState();
+
+  @override
+  void initState() {
+    super.initState();
+    _home = SchedulePage(title: _appName);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    _disposeSingletonProviders();
+  }
+
+  void _disposeSingletonProviders() {
+    MessageProvider.getInstance().dispose();
+  }
+
+  @override
+  didPopRoute() async {
+    debugPrint('back button pressed.');
+    return true;
+  }
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _appName,
+      theme: ThemeData(
+        primarySwatch: Colors.brown,
+        accentColor: Colors.orange,
+      ),
+      home: _home,
+    );
+  }
+}
+
---
+++ lib/pages/schedule/ArchivedMessages.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:message_app/models/Message.dart';
+import 'package:message_app/pages/schedule/Schedule.dart';
+
+class ArchivedMessages extends StatefulWidget {
+  final List<Message> messages;
+
+  const ArchivedMessages(this.messages);
+
+  _ArchivedMessagesState createState() => _ArchivedMessagesState();
+}
+
+class _ArchivedMessagesState extends State<ArchivedMessages> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Archived Messages'),
+      ),
+      body: Schedule(widget.messages)
+    );
+  }
+}
